34f017e1f1bd5a0592e6f93ad70d146f23d9497d
AbuBakr Humayun <68757027+abubakrhumayun@users.noreply.github.com>
2021-10-02 16:37:38 +0500

    updated

---
+++ 2/src/test/java/Arraytest.java
@@ -0,0 +1,171 @@
+
+
+import static org.junit.Assert.*;
+
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class Arraytest{
+	
+	private Array array;
+	@Before
+	public void init() {
+		array = new Array(); //create an array
+	}
+
+	@Test
+	public void test100()
+	{
+		
+		array.insert(1,0);
+		array.insert(2,2);
+		array.insert(3,3);
+		array.insert(4,4);
+		array.display();
+	}
+
+        @Test
+        public void test101()
+	{
+		
+		array.insert(100,0);
+		array.insert(102,1);
+		array.insert(103,2);
+		array.insert(104,3);
+		
+		array.delete(2);
+        array.delete(3);
+        assertFalse(array.linearSearch(3));
+	}
+
+        @Test 
+        public void test102()
+	{
+		
+		
+		array.insert(0,-1);
+		array.insert(1,1);
+		array.insert(2,2);
+		System.out.println("NullPointerException");	
+	}
+
+        @Test
+        public void test103()
+	{
+		
+        	array.insert(0,0);
+    		array.insert(1,101);
+    		array.insert(2,102);
+         	System.out.println("NullPointerException");
+		
+	}
+
+        @Test
+        public void test104()
+	{
+		
+		array.insert(100,0);
+		array.insert(102,1);
+		array.insert(103,2);
+		array.insert(104,3);
+		
+		array.delete(1);
+        assertFalse(array.linearSearch(1));
+	}
+
+        @Test
+        public void test105()
+	{
+		
+		array.insert(50,0);
+		array.insert(55,1);
+		assertTrue(array.linearSearch(55));
+	
+	}
+
+        @Test
+        public void test106()
+	{
+		array.insert(10,0);
+		array.insert(17,1);
+		array.insert(3,2);
+		array.insert(20,3);
+		array.bubbleSort();
+		
+		int[] arr = {3,10,17,20};
+		assertArrayEquals(array.array,arr);
+	}
+
+        @Test
+        public void test107()
+	{
+		
+		array.insert(10,0);
+		array.insert(17,1);
+		array.insert(3,2);
+		array.insert(20,3);
+		array.bubbleSort();
+		
+                boolean bool = false;
+		int[] newarr = {3,10,17,20};
+		
+                for(int i = 0; i < array.size;i++)
+                 {
+                      if(array.array[i] == newarr[i])
+                      {
+                          bool = true;
+                      }
+                 }
+	}
+
+        @Test
+        public void test108()
+	{
+		
+		array.insert(10,0);
+		array.insert(15,1);
+		array.insert(11,2);
+		array.insert(14,3);
+		array.display();
+		assertTrue(array.binarySearch(11, 0, array.size));
+	}
+
+       @Test
+       public void test109()
+	{
+		
+		array.insert(-6,0);
+		array.insert(-4,1);
+		array.insert(-8,2);
+		array.insert(-9,3);
+		assertTrue(array.binarySearch(-4, 0, 3));
+	}
+
+  
+
+        @Test
+        public void test110()
+	{
+		
+		array.insert(20,0);
+		array.insert(33,1);
+		array.insert(12,2);
+        array.insert(10,3);
+        array.insert(5,4);
+        array.insert(43,5);
+		
+		for(int i = 0; i < array.size; i++)
+		{
+		    if(array.array[i] == 10)
+		    {
+		       assertEquals(10,array.array[i]);
+		    }
+		}
+	
+	}
+
+}
---
+++ 2/src/test/java/Queuetest.java
@@ -0,0 +1,153 @@
+
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class Queuetest {
+
+	
+	private Queue queue;
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+	
+	@Before
+	public void init() {
+		
+		queue = new Queue();
+	}
+
+	@Test
+	public void test100() {
+		queue.enqueue(5);
+		queue.enqueue(6);
+		assertEquals(5,queue.head.key);
+	}
+	
+	@Test
+	public void test101() {
+	queue.enqueue(5);
+	queue.enqueue(6);
+	queue.display();
+	queue.dequeue();
+	assertEquals(6,queue.head.key);
+	}
+	
+	@Test
+	public void test102() {
+		
+		
+		queue.enqueue(503);
+		queue.dequeue();
+		queue.dequeue();
+		//Exception expected
+	}
+	
+	@Test
+	public void test103() {
+		for(int i=0;i<10000;i++) {
+			queue.enqueue(i);
+		}
+		assertEquals(0,queue.head.key);
+		
+	}
+	
+	@Test
+	public void test104() {
+		for(int i=0;i<10000;i++) {
+			queue.enqueue(i);
+		}
+		assertEquals(10000,queue.size);
+	}
+	
+	@Test
+	public void test105() {
+		
+		for(int i=0;i<1000;i++) {
+			queue.enqueue(i);
+		}
+		
+		for(int i=0;i<999;i++) {
+			queue.dequeue();
+		}
+		
+		assertEquals(999,queue.head.key);
+	}
+		
+	
+	@Test
+	public void test106() {
+		queue.enqueue(0);
+		queue.enqueue(1);
+		queue.enqueue(3);
+		queue.display();
+		queue.enqueue(8);
+		queue.dequeue();
+		
+		assertEquals(3,queue.size);
+		assertEquals(1,queue.head.key);
+	}
+	
+	
+	@Test
+	public void test107() {
+		
+		queue.enqueue(1);
+		
+		
+		assertNotNull(queue.head);
+		
+	}
+	
+	@Test
+	public void test108() {
+		
+		for(int i=0;i<1000;i++) {
+			queue.enqueue(i);
+		}
+		int[] array=new int [1000];
+		
+		
+		
+		for(int i=0;i<1000;i++) {
+			array[i]=queue.head.key;
+			queue.head=queue.head.next;
+			
+		}
+		for(int i=0;i<1000;i++) {
+		assertEquals(array[i],i);
+		}
+			
+		}
+		
+		@Test
+		public void test109() {
+			
+			for(int i=0;i<1000;i++) {
+				queue.enqueue(i);
+			}
+			Queue.Node temp = queue.head;
+			
+			for(int i=0;i<1000;i++) {
+				assertEquals(i,temp.key);
+				temp=temp.next;
+				
+			}
+		
+		
+	}
+		
+		@Test
+		public void test110() {
+			assertEquals(-1,queue.dequeue());
+			queue.enqueue(3);
+			assertEquals(3,queue.dequeue());
+			
+		}
+
+}
